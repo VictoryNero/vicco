292126e2a36917a06fd8f03db2f43ff578d31afc
VictoryNero <117783144+VictoryNero@users.noreply.github.com>
2022-11-09 10:36:35 +0330

    Add files via upload

---
+++ popo.js
@@ -0,0 +1,5 @@
+const fil = document.querySelector('.fil')
+const solid = document.querySelector('.solid')
+fil.addEventListener('click', function(){
+    solid.classList.toggle('lolid')
+})
---
+++ services.js
@@ -0,0 +1,48 @@
+function add1(){
+    document.calculator.screen.value += "1"
+}
+function add2(){
+    document.calculator.screen.value += "2"
+}
+function add3(){
+    document.calculator.screen.value += "3"
+}
+function add4(){
+    document.calculator.screen.value += "4"
+}
+function add5(){
+    document.calculator.screen.value += "5"
+}
+function add6(){
+    document.calculator.screen.value += "6"
+}
+function add7(){
+    document.calculator.screen.value += "7"
+}
+function add8(){
+    document.calculator.screen.value += "8"
+}
+function add9(){
+    document.calculator.screen.value += "9"
+}
+function add0(){
+    document.calculator.screen.value += "0"
+}
+function minus(){
+    document.calculator.screen.value += "-"
+}
+function add(){
+    document.calculator.screen.value += "+"
+}
+function multiply(){
+    document.calculator.screen.value += "*"
+}
+function divide(){
+    document.calculator.screen.value += "/"
+}
+function equal(){
+    document.calculator.screen.value = eval (document.calculator.screen.value)
+}
+function reset(){
+
+}
